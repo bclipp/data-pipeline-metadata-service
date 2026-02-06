8cb6ba66ec6345c6c9ed5ec61c324322c129648d
Brian L <bclipp770@gmail.com>
2026-02-06 12:05:50 -0500

    add import

--- src/main/java/com/example/datapipelinemetadata/controller/HealthController.java
+++ src/main/java/com/example/datapipelinemetadata/controller/HealthController.java
@@ -1,6 +1,7 @@
 package com.example.datapipelinemetadata.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HealthController {
