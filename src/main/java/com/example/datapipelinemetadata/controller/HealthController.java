8dc74c2711c8dbbb9305a9208ca7b9c911538a58
Brian L <bclipp770@gmail.com>
2026-02-06 12:03:54 -0500

    Update HealthController.java

--- src/main/java/com/example/datapipelinemetadata/controller/HealthController.java
+++ src/main/java/com/example/datapipelinemetadata/controller/HealthController.java
@@ -1,4 +1,19 @@
 package com.example.datapipelinemetadata.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
+
+    @GetMapping("/health")
+    public String sayHello() {
+        return "OK";
+    }
+
+    // expose new endpoint for "workout"
+    @GetMapping("/version")
+    public String workout() {
+        return "Get after it!";
+    }
+
 }
