d5508376836dd2b0b725b20a725d0c64f61d5326
Brian L <bclipp770@gmail.com>
2026-02-06 12:13:56 -0500

    added health and env controller

--- src/main/java/com/example/datapipelinemetadata/controller/HealthController.java
+++ src/main/java/com/example/datapipelinemetadata/controller/HealthController.java
@@ -1,14 +1,28 @@
 package com.example.datapipelinemetadata.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class HealthController {
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Value("${app.environment}")
+    private String appEnvironment;
 
     @GetMapping("/health")
-    public String sayHello() {
-        return "OK";
+    public String health() {
+        return "ok!";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return appName + " " + appVersion + " (" + appEnvironment + ")";
     }
 
     // expose new endpoint for "workout"
