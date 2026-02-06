ee1d60cde3cc4cd2e34479b8c73976855b1ba5ca
Brian L <bclipp770@gmail.com>
2026-02-06 11:40:29 -0500

    initial commit with skel

---
+++ src/main/java/com/yourcompany/pipeline_metadata_service/PipelineMetadataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yourcompany.pipeline_metadata_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineMetadataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineMetadataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourcompany/pipeline_metadata_service/PipelineMetadataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcompany.pipeline_metadata_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineMetadataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
